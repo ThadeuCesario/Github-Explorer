a08c0cb30d371a3213f2462c3fb0926ee1b9350b
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-05-10 23:06:29 -0300

    Route system : react-router-dom

---
+++ github-project-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+/**
+ * FC -> Function Component
+ */
+
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>
+}
+
+export default Dashboard;
---
+++ github-project-react/src/pages/Repository/index.tsx
@@ -0,0 +1,11 @@
+/**
+ * FC -> Function Component
+ */
+
+import React from 'react';
+
+const Repository: React.FC = () => {
+  return <h1>Repository</h1>
+}
+
+export default Repository;
