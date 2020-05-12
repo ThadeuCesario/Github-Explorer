66b19597a8d04f13080d25ace275ec1f0cb016ba
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-05-11 22:00:46 -0300

    Using styled components

--- github-project-react/src/App.tsx
+++ github-project-react/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 const App:React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <React.Fragment>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle/>
+  </React.Fragment>
 );
 
 export default App;
---
+++ github-project-react/src/pages/Dashboard/styles.ts
@@ -0,0 +1,12 @@
+/**
+ * Utilizando styled components
+ *
+ * Vamos trabalhar com componentes estilizados
+ */
+
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color:  #3A3A3A;
+`;
