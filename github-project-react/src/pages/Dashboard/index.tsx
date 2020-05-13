b937862c514dd07e2683851cce9c25a430298f91
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-05-12 22:55:25 -0300

    Saving infos in localStorage

--- github-project-react/src/pages/Dashboard/index.tsx
+++ github-project-react/src/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@
  * FC -> Function Component
  */
 
-import React, {useState, FormEvent} from 'react';
+import React, {useState, useEffect, FormEvent} from 'react';
 import {FiChevronRight} from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -16,6 +16,10 @@ import {Title, Form, Repositories, Error} from './styles';
  *
  * Lembre-se que precisamos colocar na tipagem SOMENTE AS INFORMAÇÕES que serão
  * utilizadas.
+ *
+ * Lembre-se que usamos o useEffect para disparamos uma função, sempre que uma determinada
+ * variável for alterada.
+ * useEffect(() => {}, []);
  */
 
 interface Repository{
@@ -30,7 +34,29 @@ interface Repository{
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+    if(storagedRepositories){
+      /**
+       * Como convertemos anteriormente, precisamos realizar outra conversão
+       * para transformá-lo em um array.
+       */
+      return JSON.parse(storagedRepositories)
+    }
+    else{
+      return [];
+    }
+  });
+
+  /**
+   * Sempre que eu tiver uma mudança em 'repositories', estou armazenando
+   * a informação em localStorage. Porém como não consigo armazenar um array inteiro,
+   * estou convertendo primeiramente para uma string.
+   */
+  useEffect(() => {
+    localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories));
+  }, [repositories]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void>{
     /**
