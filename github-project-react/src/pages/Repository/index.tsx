9ae4576866a36595e9736d563d17522a97a43fc3
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-05-13 22:54:48 -0300

    Github Explorer Project - React with Typescript

--- github-project-react/src/pages/Repository/index.tsx
+++ github-project-react/src/pages/Repository/index.tsx
@@ -2,9 +2,10 @@
  * FC -> Function Component
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useRouteMatch, Link} from 'react-router-dom';
 import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+import api from '../../services/api';
 
 /**
  * Dentro de useRouteMatch, teremos o parâmetro da rota.
@@ -13,13 +14,58 @@ import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
 
   import logoImg from '../../assets/logo.svg';
   import {Header, RepositoryInfo, Issues} from './styles';
+import { promises } from 'fs';
 
   interface RepositoryParams{
     repository: string;
   }
 
+  interface Repository{
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+      login: string;
+      avatar_url: string;
+    }
+  }
+
+  interface Issue{
+    id: number;
+    title: string;
+    html_url: string;
+    user: {
+      login: string;
+    }
+  }
+
   const Repository: React.FC = () => {
+    const [repository, setRepository] = useState<Repository | null>(null); //Inicia com null, porque não terá nada carregado no começo.
+    const [issues, setIssues] = useState<Issue[]>([]);
+
     const {params} = useRouteMatch<RepositoryParams>();
+
+    /**
+     * Sempre utilizamos o useEffect quando desejamos disparar alguma ação,
+     * quando algo acontecer.
+     *
+     * Quando estamos realizando uma chamada a uma API no react, precisamos
+     * passá-la como parâmetro de mudança. Isso porque , caso essa variável tenha
+     * alguma alteração. Precisaremos que a chamada na API seja feita novamente.
+     */
+    useEffect(() => {
+      api.get(`repos/${params.repository}`)
+        .then(response => {
+          setRepository(response.data);
+        });
+
+      api.get(`repos/${params.repository}/issues`)
+        .then(response => {
+          setIssues(response.data);
+        });
+    }, [params.repository]);
 
     return (
       <React.Fragment>
@@ -31,43 +77,47 @@ import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
           </Link>
         </Header>
 
-        <RepositoryInfo>
-          <header>
-            <img src="https://avatars1.githubusercontent.com/u/27692953?v=4" alt="Thadeu" />
-            <div>
-              <strong>
-                thadeucesario/javascript
-              </strong>
-              <p>
-                descrição do repositório.
-              </p>
-            </div>
-          </header>
-          <ul>
-            <li>
-              <strong>1808</strong>
-              <span>Stars</span>
-            </li>
-            <li>
-              <strong>48</strong>
-              <span>Forks</span>
-            </li>
-            <li>
-              <strong>67</strong>
-              <span>Issues abertas</span>
-            </li>
-          </ul>
-        </RepositoryInfo>
+        { repository && (
+          <RepositoryInfo>
+            <header>
+              <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+              <div>
+                <strong>
+                  {repository.full_name}
+                </strong>
+                <p>
+                  {repository.description}
+                </p>
+              </div>
+            </header>
+            <ul>
+              <li>
+                <strong>{repository.stargazers_count}</strong>
+                <span>Stars</span>
+              </li>
+              <li>
+                <strong>{repository.stargazers_count}</strong>
+                <span>Forks</span>
+              </li>
+              <li>
+                <strong>{repository.open_issues_count}</strong>
+                <span>Issues abertas</span>
+              </li>
+            </ul>
+          </RepositoryInfo>
+        )}
 
         <Issues>
-        <Link to={`qualquer coisa`}>
-            <div>
-              <strong>qualquer coisa</strong>
-              <p>qualquer coisa</p>
-            </div>
+          {issues.map(issue => (
+            <a key={issue.id} href={issue.html_url}>
+              <div>
+                <strong>{issue.title}</strong>
+                <p>{issue.user.login}</p>
+              </div>
 
-            <FiChevronRight size={20} />
-          </Link>
+              <FiChevronRight size={20} />
+            </a>
+          ))}
         </Issues>
       </React.Fragment>
     );
